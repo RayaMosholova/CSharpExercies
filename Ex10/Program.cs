dddfccbc18e8ab120fb9970bbef9d34dd185a97d
Raya Mosholova <raya.mosholova@gmail.com>
2021-04-03 17:17:46 +0300

    Adding few more exercises

---
+++ Ex10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte centuries = byte.Parse(Console.ReadLine());
+            short years = (short)(centuries * 100);
+            int days = (int)(years * 365.2422);
+            int hours = days * 24;
+            long minutes = hours * 60;
+            long seconds = minutes * 60;
+            long miliseconds = seconds * 1000;
+            long microseconds = miliseconds * 1000;
+            long nanoseconds = microseconds * 1000;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {miliseconds} miliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds");
+        }
+    }
+}
---
+++ Ex8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("First name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Age: ");
+            byte age = byte.Parse(Console.ReadLine());
+            Console.Write("Gender (m or f): ");
+            char gender = char.Parse(Console.ReadLine());
+            Console.Write("Personal ID number: ");
+            long personalIdNumber = long.Parse(Console.ReadLine());
+            Console.Write("Unique employee number: ");
+            int uniqueEmployeeNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"First name: {firstName}");
+            Console.WriteLine($"Last name: {lastName}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Gender: {gender}");
+            Console.WriteLine($"Personal ID: {personalIdNumber}");
+            Console.WriteLine($"Unique Employee number: {uniqueEmployeeNumber}");
+        }
+    }
+}
